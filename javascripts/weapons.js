f0d5923932c13de5948e695ff05fd288768f1396
Zack Repass <jzrepass@gmail.com>
2016-08-10 16:50:59 -0500

    small update

--- javascripts/weapons.js
+++ javascripts/weapons.js
@@ -1,7 +1,4 @@
-var Gauntlet || {};
-Gauntlet.WeaponsCloset = {};
-
-Gauntlet.WeaponsCloset.Weapon = function() {
+var Weapon = function() {
   this.name = "bare hands";
   this.damage = 1;
   this.hands = 2;
@@ -11,24 +8,24 @@ Gauntlet.WeaponsCloset.Weapon = function() {
   }
 };
 
-Gauntlet.WeaponsCloset.Dagger = function() {
+var Dagger = function() {
   this.name = "dagger";
   this.damage = 4;
   this.hands = 1;
 };
-Dagger.prototype = new Gauntlet.WeaponsCloset.Weapon();
+Dagger.prototype = new Weapon();
 
 var BroadSword = function() {
   this.name = "broad sword";
   this.damage = 14;
   this.hands = 2;
 };
-BroadSword.prototype = new Gauntlet.WeaponsCloset.Weapon();
+BroadSword.prototype = new Weapon();
 
 var WarAxe = function() {
   this.name = "war axe";
   this.damage = 18;
   this.hands = 2;
 };
-WarAxe.prototype = new Gauntlet.WeaponsCloset.Weapon();
+WarAxe.prototype = new Weapon();
 
