f2e478961562193cc5ea29c42508b9d4be6f37dd
Fletcher Watson <fbwthe6@gmail.com>
2016-08-12 14:10:51 -0500

    put setWeapons back into weapons.js but commented it out.

--- javascripts/weapons.js
+++ javascripts/weapons.js
@@ -12,6 +12,9 @@ Gauntlet.WeaponsCloset.Weapon = function () {
     return "" + this.name + " " + this.type + " attack for " + this.damage + " damage!";
   }
 };
+// Gauntlet.Combatants.Player.prototype.setWeapon = function(newWeapon) {
+//   this.weapon = new Gauntlet.WeaponsCloset[newWeapon];
+// }
 
 //FIGHTER CLASS WEAPONS
 Gauntlet.WeaponsCloset.KnottedClub = function() {
